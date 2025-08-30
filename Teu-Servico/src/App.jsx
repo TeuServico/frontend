6c746505d13567ab9cfb65999c5e764e79561355
Lazarokaua <lazarokaua4@gmail.com>
2025-08-30 10:45:09 -0300

    :sparkles: feat(header): Adicionado header do projeto

--- Teu-Servico/src/App.jsx
+++ Teu-Servico/src/App.jsx
@@ -3,11 +3,13 @@ import CreateAccount from './pages/Auth/CreateAccount/CreateAccount';
 import Login from './pages/Auth/Login/Login';
 import ForgotPassword from './pages/Auth/ForgotPassword/ForgotPassword';
 import ResetPassword from './pages/Auth/ResetPassword/ResetPassword';
+import { Header } from './components/Header';
+import "./index.css"
 
 function App() {
   return (
     <div>
-      <ResetPassword />
+      <Header />
     </div>
   );
 }

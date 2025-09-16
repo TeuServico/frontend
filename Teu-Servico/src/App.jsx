445e25d5d6998987f7cd79a1630d15ff504c6487
klug-estudos <rodrigoklug@gmail.com>
2025-09-15 22:14:32 -0300

    feat: adiciona Hero.jsx e atualiza App.jsx

--- Teu-Servico/src/App.jsx
+++ Teu-Servico/src/App.jsx
@@ -4,12 +4,14 @@ import Login from './pages/Auth/Login/Login';
 import ForgotPassword from './pages/Auth/ForgotPassword/ForgotPassword';
 import ResetPassword from './pages/Auth/ResetPassword/ResetPassword';
 import { Header } from './components/Header';
+import { Hero } from "./components/Hero";
 import "./index.css"
 
 function App() {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 }
